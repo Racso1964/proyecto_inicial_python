fd09431be9b9189719d3297086751122450b7fc7
Racso1964 <142761936+Racso1964@users.noreply.github.com>
2023-10-09 00:31:38 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import random
 
+# randon.choise devuelve un velor aleatorio extraido de la secuencia pasada como argumento
+
+import interfaz
+
+
+
+
+def  leer_palabra_secreta(palabras):
+
+
+
+    
+    pass
+
+if __name__ == "__main__":
+    pass
